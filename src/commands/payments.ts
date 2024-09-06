2ff410419a8bd8208cc871130bb1c2bf31b1c3cc
tommy-pellerin <tommytp@hotmail.fr>
2024-09-06 15:36:45 +0200

    can check transaction ID before sending notification

--- src/commands/payments.ts
+++ src/commands/payments.ts
@@ -1,9 +1,7 @@
-import { CommandInteraction, SlashCommandBuilder, Message } from "discord.js";
+import { CommandInteraction, SlashCommandBuilder, Message, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { getMotionStakingStartedMentions, getMotionStakingStartedEmbed }  from "../embed_builders/motionStakingStartedEmbed"
 import { storeDiscordNotification } from "../script";
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { verifyIfNotificationSent } from "../utilities/verifyNotificationsInDB";
 
 export const data = new SlashCommandBuilder()
     .setName("payments")
@@ -11,46 +9,40 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction: CommandInteraction, timestamp: number) {
 
+    const alreadyNotified = await verifyIfNotificationSent("1281557392sdvsdv592736308")
+    if(alreadyNotified){
+      console.log("This notification has already been sent by the bot");
+      return;
+    }
+
     // Mention the user who triggered the command
     const userId = interaction.user.id;
 
     // Create the embed
     const embed = getMotionStakingStartedEmbed(timestamp)
     const mentions = getMotionStakingStartedMentions()
-
+    // Define the component
+    const component = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+          .setURL("https://coconut-harrier-5b4.notion.site/Colony-Notification-discord-bot-d4f2334903a4481d9c11253b7ed9c808")
+          .setStyle(ButtonStyle.Link)
+          .setLabel("View transaction on Colony")
+          .setDisabled(false)
+    );
+    
     // Send the reply and get the message object
     const message: Message = await interaction.reply({
       content: `<@${userId}> ${mentions}`,
       embeds: [embed],
-      components: [
-        {
-          type: 1,
-          components: [
-            {
-              url: `https://coconut-harrier-5b4.notion.site/Colony-Notification-discord-bot-d4f2334903a4481d9c11253b7ed9c808`,
-              style: 5,
-              label: "View transaction on Colony",
-              disabled: false,
-              type: 2,
-            },
-          ],
-        },
-      ],
+      components: [component],
       fetchReply: true, // Ensure the reply is fetched
     });
 
     // Log or use the message information
     console.log("Notification sent:");
     // console.log(message);
-    console.log(`Message ID: ${message.id}`);
-    console.log(`Channel ID: ${message.channel.id}`);
+    // console.log(`Message ID: ${message.id}`);
+    // console.log(`Channel ID: ${message.channel.id}`);
     await storeDiscordNotification(message)
-      .then(async () => {
-        await prisma.$disconnect()
-      })
-      .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-      })
+
 }
--- src/script.ts
+++ src/script.ts
@@ -4,13 +4,20 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function storeDiscordNotification(discordAnswer: Message) {
-  const notification = await prisma.notification.create({
-    data: {
-      transactionId:  discordAnswer.id,
-      discordLink: discordAnswer.channel.id,
-    },
-  })
-  console.log("🎉 Les notifications sont bien enregistrées");
-  console.log(notification)
+  try {
+    const notification = await prisma.notification.create({
+      data: {
+        transactionId: discordAnswer.id,
+        discordLink: discordAnswer.channel.id,
+      },
+    });
+    console.log("🎉 Les notifications sont bien enregistrées");
+    console.log(notification);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
